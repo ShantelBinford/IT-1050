5d4475bd3450e8e82fb0342d5cef213f9bfb1fd3
shantelbinford <s00842941@acad.tri-c.edu>
2019-09-24 20:52:02 -0400

    Initial check-in of module assignment1

---
+++ Assignment1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.) Compute Full Name (String) [ check ]
+            // 2.) Compute Height CM (double)
+            // 3.) Compute if you Can Vote (bool)
+
+
+            // 1
+
+            string firstName;
+            string middleName;
+            string lastName;
+
+            Console.WriteLine("Shantel");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("DeSha");
+            middleName = Console.ReadLine();
+
+            Console.WriteLine("Binford");
+            lastName = Console.ReadLine();
+
+            string fullName = firstName + " " + middleName + " " + lastName;
+
+            Console.WriteLine(fullName);
+
+            Console.ReadKey();
+
+            // 2
+
+            const double CM_MULTIPLER = 2.54;
+            const double INCHES_PER_FOOT = 12;
+
+            int heightFeet;
+            double heightInches;
+
+            Console.WriteLine("5: ");
+            heightFeet = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("6: ");
+            heightInches = double.Parse(Console.ReadLine());
+
+            double inches = heightFeet * INCHES_PER_FOOT;
+
+            inches += heightInches;
+
+            double heightCM = CM_MULTIPLER * inches;
+
+            Console.WriteLine(heightCM);
+
+            // 3
+
+            int age;
+            bool isCitizen;
+
+            Console.WriteLine("28");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("yes");
+            bool = Console.ReadLine().ToLower().StartsWith("y");
+
+            Console.WriteLine(age >= 18 && isCitizen);
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
